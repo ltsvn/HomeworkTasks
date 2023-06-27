b36c43ef5bab3120d01cc907d7a744c2046b2f39
ltsvn <92997990+ltsvn@users.noreply.github.com>
2023-06-27 17:50:29 +0200

    fix active state of the link

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -1,29 +1,30 @@
-import React, {FC} from 'react'
-import {NavLink} from 'react-router-dom'
-import s from './Sidebar.module.css'
-import {PATH} from '../Pages'
-import closeIcon from './closeOutline.svg'
+import React, { FC } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import s from './Sidebar.module.css';
+import { PATH } from '../Pages';
+import closeIcon from './closeOutline.svg';
 
 type PropsType = {
-    open: boolean
-    handleClose: () => void
-}
+    open: boolean;
+    handleClose: () => void;
+};
 
-export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
-    const sidebarClass = s.sidebar
-        + (open ? ' ' + s.open : '')
+export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
+    const sidebarClass = s.sidebar + (open ? ' ' + s.open : '');
+    const location = useLocation(); // Get the current location object from react-router-dom
+
+    const isLinkActive = (path: string) => {
+        return location.pathname === path; // Check if the link's path matches the current pathname
+    };
+
     return (
         <>
-            {/*затемнение справа от открытого меню*/}
-            {open && <div className={s.background} onClick={handleClose}/>}
+            {/* затемнение справа от открытого меню */}
+            {open && <div className={s.background} onClick={handleClose} />}
 
             <aside className={sidebarClass}>
                 <button className={s.close} onClick={handleClose}>
-                    <img
-                        src={closeIcon}
-                        alt="close sidebar"
-                        id={'hw5-menu-close'}
-                    />
+                    <img src={closeIcon} alt="close sidebar" id={'hw5-menu-close'} />
                 </button>
 
                 <nav id={'hw5-menu'} className={s.nav}>
@@ -31,7 +32,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={({ isActive }) => (`${isActive} ? ${s.active} : ${s.a}'`)} // делает студент
+                        className={`${s.a} ${isLinkActive(PATH.PRE_JUNIOR) ? s.active : ''}`}
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +40,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={({ isActive }) => (`${isActive} ? ${s.active} : ${s.a}'`)} // делает студент
+                        className={`${s.a} ${isLinkActive(PATH.JUNIOR) ? s.active : ''}`}
                     >
                         Junior
                     </NavLink>
@@ -47,12 +48,12 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={({ isActive }) => (`${isActive} ? ${s.active} : ${s.a}'`)}// делает студент
+                        className={`${s.a} ${isLinkActive(PATH.JUNIOR_PLUS) ? s.active : ''}`}
                     >
                         Junior Plus
                     </NavLink>
                 </nav>
             </aside>
         </>
-    )
-}
+    );
+};
